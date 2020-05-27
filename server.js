e0f942410a16ced9844fe7e17e01dc6bc0c8c451
angebrito <49460211+angebrito@users.noreply.github.com>
2020-05-27 10:42:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require ('express');
 
 const server = express();
 
+server.use(express.json());
 
 //CRUD
 
+//GET
 const users = ['Angelica', 'Yusi', 'Freya'];
 
 server.get('/users', (req, res) => {
@@ -15,6 +17,28 @@ server.get ('/users/:index', (req, res) =>  {
     const {index} = req.params;
 
     return res.json(users[index]);
+})
+
+//POST (CRIAÇÂO)
+
+server.post('/users', (req, res) => {
+    const { name } = req.body;
+    
+    users.push(name);
+    
+    return res.json(users);
+})
+
+
+//PUT (EDIÇÂO)
+
+server.put('/users/:index', (req, res ) => {
+    const { index } = req.body;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
 })
 
 server.listen(3000);
